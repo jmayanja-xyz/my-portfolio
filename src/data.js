5cbe1278db19fe0a4a6f8bc961f4951702bed576
jmayanja-xyz <51414069+jmayanja-xyz@users.noreply.github.com>
2022-02-08 23:51:45 +0000

    Completed first version of site

--- src/data.js
+++ src/data.js
@@ -2,35 +2,40 @@ export const projects = [
   {
     title: "Final Year Project",
     subtitle: "Voting dApp on Rinkeby Testnet",
-    description: "",
+    description:
+      "My final year project was based on building a Truffle-React dApp that would allow voters to securely cast votes. The solution the project aimed to solve was reducing electoral fraud. All the transaction would be deployed on the Rinkeby Testnet. (Can also be configured to be deployed on local Ganache blockchain)",
     image: "./dApp.gif",
     link: "https://github.com/jmayanja-xyz/fyp_voting_dApp",
   },
   {
     title: "Playtech - Ryota",
     subtitle: "Front End Developer",
-    description: "",
+    description:
+      "I was a front end developer at Playtech for 13 months. My job was to primarily work on an application called Ryota (gambling game editor) by building features, fixing bugs and migrating the system from Angular Js to Angular.",
     image: "./PT-Hero-Animated.gif",
     link: "https://www.playtech.com/",
   },
   {
     title: "Year 2 - Group Android Project",
     subtitle: "Laundry App",
-    description: "",
+    description:
+      "First group full-stack project where we attempted to make an Android app that would effectively be UberEats but for washing clothes. My role was designing features such as the RecyclerView using Java and XML and connecting the project to Firebase",
     image: "./Yr2_group_Project.png",
     link: "https://github.com/BrunelCS",
   },
   {
     title: "Year 2 - Project Prototype",
     subtitle: "Laundry App Adobe XD",
-    description: "",
+    description:
+      "The prototype for the Android Laundry project. Due to my experience with design my rolewas to design the whole project in Adobe XD as a reference for my group members.",
     image: "./Adobe_XD.gif",
     link: "https://xd.adobe.com/view/2108e5e4-847c-4d51-7963-8442aa631fdf-5739/grid/",
   },
   {
     title: "Year 1 - Finch Group Project",
     subtitle: "Eclipse Java Project",
-    description: "",
+    description:
+      "One of my first technical group projects, where we each had to make a finch robot do certain movements. My job was to get it to move in zig-zag patterns.",
     image: "./Finch.gif",
     link: "https://store.birdbraintechnologies.com/",
   },
@@ -38,7 +43,8 @@ export const projects = [
   {
     title: "Software Project Managemnet Prototype",
     subtitle: "Student Banking App - Marvel App",
-    description: "",
+    description:
+      "A group project where we had to make prototypes of a student baking app. My role was to implement the 'homepage' and the 'view statements' page. ",
     image: "./Marvel_Banking.gif",
     link: "https://marvelapp.com/prototype/16269824",
   },
@@ -46,9 +52,17 @@ export const projects = [
     title: "Year 2 - Group Usability Project",
     subtitle: "Banking System for Older People",
     description:
-      "A group projet where we had to demo a TV system for the elderly. My role was to help make designs, build the prototype in MS Powerpoint and make a presentation ",
+      "A group project where we had to demo a TV system for the elderly. My role was to help make designs, build the prototype in MS Powerpoint and make a presentation ",
     image: "./Elderly_Application.gif",
     link: "https://www.youtube.com/watch?v=B3-GrL2z-yE",
+  },
+  {
+    title: "Hackathon Project",
+    subtitle: "Pokemon Viewer",
+    description:
+      "Developed this project at a hackathon workshop, which is a site that is a pokemon viewer using Glitch, Flask & Python. You can search for the pokemon and it would return with a short encyclopedia about them.",
+    image: "./pokemon-glitch.gif",
+    link: "https://josiahs-pokemon-viewer.glitch.me/",
   },
 ];
 
@@ -58,10 +72,6 @@ export const skills = [
   "TypeScript",
   "HTML",
   "CSS",
-  "Sass",
-  "React",
-  "Angular",
-  "Truffle",
   "Node.js",
   "Material UI",
   "React Bootstrap",
@@ -77,4 +87,8 @@ export const skills = [
   "Adobe XD",
   "Marvel App",
   "Agile",
+  "Glitch",
+  "Flask",
+  "Python",
+  "Firebase",
 ];
